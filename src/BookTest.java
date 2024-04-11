7e1a4afc615dbe0583dd9663980965c2b1b9c9cd
Simon Nestler <simon.nestler@thi.de>
2024-04-11 10:21:54 +0200

    Teilaufgabe 3 geloest

--- src/BookTest.java
+++ src/BookTest.java
@@ -6,13 +6,27 @@ public class BookTest {
         books[0].setTitle("The Catcher in the Rye");
         books[0].setAuthor("J.D. Salinger");
         books[0].setPages(234);
-        books[0].setPrice(19.99f);
+        books[0].setPrice(19.90f);
 
-        books[2] = new Book();
-        books[2].setTitle("The Great Gatsby");
-        books[2].setAuthor("F. Scott Fitzgerald");
-        books[2].setPages(180);
-        books[2].setPrice(14.99f);
+        books[1] = new Book();
+        books[1].setTitle("The Great Gatsby");
+        books[1].setAuthor("F. Scott Fitzgerald");
+        books[1].setPages(180);
+        books[1].setPrice(14.99f);
 
+        BookTest.printBooks(books);
+
+    }
+
+    public static void printBooks(Book[] books) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null) {
+                System.out.print(books[i].getAuthor() + ", ");
+                System.out.print(books[i].getTitle() + ", ");
+                System.out.print(books[i].getPages() + "p., ");
+                System.out.println(books[i].getPrice() + " Eur");
+            }
+
+        }
     }
 }
